300aac749f12364ac465d56b39468f52a7f68d9c
emilmarcel <55388306+marcelcph@users.noreply.github.com>
2023-05-18 11:06:43 +0200

    Start

---
+++ cm-leag-gen/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <html data-theme="cupcake">
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1> 
+      </html>
+    </>
+    
+  )
+}
+
+export default App
